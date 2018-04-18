54c1a0e12b3b5cf1826326692c46537956c060d7
Cordero <cord.a.hernandez@gmail.com>
2018-04-18 12:08:14 -0700

    Adds ArticlesCollectionViewCell

---
+++ Views/ArticlesCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArticlesCollectionViewCell.swift
+//  Nuse
+//
+//  Created by Cordero Hernandez on 4/16/18.
+//  Copyright © 2018 Cordero Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesCollectionViewCell: UICollectionViewCell {
+    
+    
+}
